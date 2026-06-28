9c4ae91e0718cd5bed0ea54a2057c9d77f799567
Mohamed Fathy <drmohamedalangr@gmail.com>
2026-06-28 16:56:50 +0300

    Fix streamlit python path

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add project root to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 import streamlit as st
 from src.dashboard.state import init_state
 
